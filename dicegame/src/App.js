fe01527690779e72a6d21535152ceb8dfc9ece80
Diarit Salihaj <diarit-94@hotmail.fr>
2021-12-14 15:18:39 +0100

    ctnue

--- dicegame/src/App.js
+++ dicegame/src/App.js
@@ -3,12 +3,25 @@ import './App.css';
 import './assets/Styles/main.scss'
 
 import TheButton from './components/TheButton/TheButton.js';
+import InputGroup from './components/InputGroup/InputGroup.js';
+
+import React from 'react'
+
+export const GameConfig = {
+  diceNbs: 0
+}
+
+export const GameContext = React.createContext();
 
 const App = () => {
   return (
-    <div className="App">
-      <TheButton label="Jouer"></TheButton>
-    </div>
+    <GameContext.Provider value={GameConfig}>
+      <div className="App">
+        <TheButton label="Jouer"></TheButton>
+        <InputGroup label="Nombre de face" type="number"></InputGroup>
+        <InputGroup label="Nombre de dés" type="number"></InputGroup>
+      </div>
+    </GameContext.Provider>
   );
 }
 
